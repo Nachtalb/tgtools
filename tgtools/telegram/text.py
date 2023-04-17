cd83f2b0f02c53f3546759601da3bc5a43961a0a
Nachtalb <na@nachtalb.io>
2023-04-17 22:20:27 +0200

    Add host emoji

--- tgtools/telegram/text.py
+++ tgtools/telegram/text.py
@@ -1,6 +1,9 @@
 import re
 from typing import Iterable
 
+from yarl import URL
+from emoji import emojize
+
 
 def tagify(tags: Iterable[str] | str) -> set[str]:
     """
@@ -43,3 +46,44 @@ def tagify(tags: Iterable[str] | str) -> set[str]:
 
     # Add a hashtag to each tag, and prefix tags starting with a digit with an underscore
     return {f"#_{tag}" if tag[0].isdigit() else f"#{tag}" for tag in filter(None, tags)}
+
+
+def host_emoji(url: str | URL) -> str:
+    """
+    Return a matching emoji for various art hosting sites.
+
+    Args:
+        url (str | URL): The URL of the art hosting site.
+
+    Returns:
+        str: The matching emoji for the provided site.
+
+    Examples:
+        >>> host_emoji("https://twitter.com")
+        '🐦'
+        >>> host_emoji("https://artstation.com")
+        '🎨'
+        >>> host_emoji("https://danbooru.donmai.us")
+        '📦'
+        >>> host_emoji("https://pixiv.net")
+        '🅿️'
+    """
+    url = URL(url)
+
+    match url.host:
+        case "twitter.com" | "www.twitter.com" | "pbs.twimg.com":
+            return emojize(":bird:")
+        case "artstation.com" | "www.artstation.com":
+            return emojize(":art:")
+        case "danbooru.donmai.us":
+            return emojize(":package:")
+        case "pixiv.net" | "www.pixiv.net" | "i.pximg.net":
+            return emojize(":parking:")
+        case "deviantart.com" | "www.deviantart.com" | "pre00.deviantart.net":
+            return emojize(":pencil:")
+        case "instagram.com" | "www.instagram.com" | "scontent.cdninstagram.com":
+            return emojize(":camera:")
+        case "tumblr.com" | "www.tumblr.com" | "assets.tumblr.com":
+            return emojize(":woman_dancing:")
+        case _:
+            return emojize(":frame_with_picture:")
