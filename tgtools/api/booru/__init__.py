d8b6f4b45549cb67c879c19d2704f1cda56328c7
Nachtalb <na@nachtalb.io>
2023-04-23 17:34:43 +0200

    Generically create sub clases of V1Apis

--- tgtools/api/booru/__init__.py
+++ tgtools/api/booru/__init__.py
@@ -1,6 +1,5 @@
 from tgtools.api.booru.danbooru import DanbooruApi
-from tgtools.api.booru.v1 import V1Api
-from tgtools.api.booru.yandere import YandereApi
+from tgtools.api.booru.v1 import V1Api, YandereApi
 
 __all__ = ["YandereApi", "DanbooruApi", "V1Api"]
 
--- tgtools/api/booru/v1.py
+++ tgtools/api/booru/v1.py
@@ -1,9 +1,28 @@
 from typing import Generic, Type
 
 from aiohttp import BasicAuth, ClientSession
+from attr import dataclass
 
+from tgtools.api.booru import HOSTS
 from tgtools.api.booru.base import BooruApi, T_Post
+from tgtools.models.booru.yandere import YanderePost
 from tgtools.utils.urls.builder import URLTemplateBuilder
+
+
+@dataclass
+class MinorVersion:
+    post_api_path: str
+    posts_api_path: str
+
+
+class GelbooruStyleVersion(MinorVersion):
+    post_api_path = "/index.php?page=dapi&s=post&q=index&json=1&id={id}"
+    posts_api_path = "/index.php?page=dapi&s=post&q=index&json=1"
+
+
+class YandereStyleVersion(MinorVersion):
+    post_api_path = "/post.json?tags=id:{id}"
+    posts_api_path = "/post.json"
 
 
 class V1Api(Generic[T_Post], BooruApi[T_Post]):
@@ -21,12 +40,15 @@ class V1Api(Generic[T_Post], BooruApi[T_Post]):
         host: str,
         session: ClientSession,
         _post_class: Type[T_Post],
+        minor_version: Type[MinorVersion] = GelbooruStyleVersion,
         auth: BasicAuth | None = None,
     ):
         super().__init__(host=host, session=session, _post_class=_post_class, auth=auth)
 
-        self._post_url = URLTemplateBuilder(f"{self.url}/post.json?tags=id:{{id}}")
-        self._posts_url = URLTemplateBuilder(f"{self.url}/post.json")
+        self.minor_version = minor_version
+
+        self._post_url = URLTemplateBuilder(f"{self.url}/{minor_version.post_api_path}")
+        self._posts_url = URLTemplateBuilder(f"{self.url}/{minor_version.posts_api_path}")
 
     async def post(self, id: int) -> T_Post | None:
         """
@@ -42,3 +64,30 @@ class V1Api(Generic[T_Post], BooruApi[T_Post]):
         if (post := await self._request(url)) and isinstance(post, list):
             return self._convert_post(post[0])
         return None
+
+
+def create_v1_api_subclass(
+    class_name: str, post_class: Type[T_Post], minor_version: Type[MinorVersion], default_host: str
+) -> Type[V1Api[T_Post]]:
+    # Define the custom __init__ method with type annotations
+    def custom_init(
+        self,
+        session: ClientSession,
+        auth: BasicAuth | None = None,
+    ) -> None:
+        V1Api.__init__(
+            self,
+            host=default_host,
+            session=session,
+            _post_class=post_class,
+            minor_version=minor_version,
+            auth=auth,
+        )
+
+    # Define a new class with the provided name, inheriting from V1Api
+    new_class = type(class_name, (V1Api[T_Post],), {"__init__": custom_init})
+
+    return new_class
+
+
+YandereApi = create_v1_api_subclass("YandereApi", YanderePost, YandereStyleVersion, HOSTS.yandere)
